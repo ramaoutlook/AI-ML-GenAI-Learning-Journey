972fa1c2e2f5e8f338a59b1e3569e4768c9eb530
ramaoutlook <47940749+ramaoutlook@users.noreply.github.com>
2025-04-14 22:56:30 +0530

    Day52 Progress second try

--- WebScrappingPython/claudeAISupplyBotChallenge/config.py
+++ WebScrappingPython/claudeAISupplyBotChallenge/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API keys and configurations
-OPENAI_API_KEY = os.getenv('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')
+OPENAI_API_KEY = os.getenv('apikey')
